4806c2a7c2e58b1bee2f38ab89ba87f3998d74cf
arcsector <26469747+arcsector@users.noreply.github.com>
2024-04-23 19:08:59 -0700

    README options and choices, updating packaging, tgz ignore

--- package_app.sh
+++ package_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 VERSION=$(grep version ./mattermost_alerts/default/app.conf | head -1 | sed 's/version = //' | sed 's/\n//')
-tar -czvf "mattermost_alerts_$VERSION.tgz" mattermost_alerts/
+tar -czvf "mattermost_alerts_${VERSION//./}.tgz" mattermost_alerts/
