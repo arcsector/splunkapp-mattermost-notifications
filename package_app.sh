9bd48cddd24cf2b2762ffc973af3fae967e6ddbc
arcsector <26469747+arcsector@users.noreply.github.com>
2024-05-13 18:13:22 -0700

    Moving everything to app dir & app manifest finish

--- package_app.sh
+++ package_app.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-APP_DIR=mattermost_alert_action
-VERSION=$(grep version "./$APP_DIR/default/app.conf" | head -1 | sed 's/version = //' | sed 's/\n//')
-tar -czvf "${APP_DIR}_${VERSION//./}.tgz" "$APP_DIR/"
+APP_DIR=TriplePlay-mattermost_alert_action
+if ! slim validate $APP_DIR; then
+  echo "App Validation Failed" > /dev/stderr
+  exit 1
+fi
+slim package $APP_DIR
